6da562b8cf23942d79b1da1d03fe66e404d0c2d0
fhoinaski <sukinodoncai@gmail.com>
2023-10-14 21:01:32 -0300

    Update server.js
    update cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ class Server{
       
       app.use(cors({
         origin: (origin, callback) => {
-          const allowedOrigins = [`http://localhost:${config.port},https://monitorrindodeploymaster.cyclic.app/`]; 
+          const allowedOrigins = ['http://localhost:${config.port}', 'https://monitorrindodeploymaster.cyclic.app'];
           if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
           } else {
@@ -29,6 +29,7 @@ class Server{
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true
       }));
+      
       app.use(express.json()) 
     }
   
